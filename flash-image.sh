aa44ad799ace5b8e579eae7254914248d5614b5b
nd <git@notandy.de>
2022-12-25 02:05:18 +0100

    add c3 image

--- flash-image.sh
+++ flash-image.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 esptool.py --port /dev/ttyUSB0 erase_flash
-esptool.py --chip esp32 --port /dev/ttyUSB0 write_flash -z 0x1000 images/esp32-20220618-v1.19.1.bin
+#esptool.py --chip esp32 --port /dev/ttyUSB0 write_flash -z 0x1000 images/esp32-20220618-v1.19.1.bin
+esptool.py --chip esp32c3 --port /dev/ttyUSB0 --baud 460800 write_flash -z 0x0 images/esp32c3-20220618-v1.19.1.bin
